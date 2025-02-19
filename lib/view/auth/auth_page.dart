1088acfe2c9375ad3854851a6cfd2f5631400186
kardelenkeles <kar_di44@hotmail.com>
2025-02-19 21:19:32 +0300

    auth page design changed

--- lib/view/auth/auth_page.dart
+++ lib/view/auth/auth_page.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:beier_app2/constants/app_constants.dart';
 import 'package:beier_app2/view/auth/viewmodel/auth_viewmodel.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
@@ -16,127 +16,153 @@ class _AuthPageState extends State<AuthPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _isLogin = false;
+  bool _isLoading = false;
+
+  static const Color primaryColor = Color(0xffBAE162);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                // Lottie Animasyonu
-                Lottie.asset(
-                  _isLogin
-                      ? AppConstants.loginAnimation
-                      : AppConstants.registerAnimation,
-                  height: 250,
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(height: 20),
-                Text(
-                  _isLogin
-                      ? AppConstants.welcomeText
-                      : AppConstants.registerText,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: AppConstants.emailHint,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(
+          _isLogin ? AppConstants.welcomeText : AppConstants.registerText,
+        ),
+      ),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AnimatedSwitcher(
+                    duration: Duration(milliseconds: 500),
+                    transitionBuilder: (widget, animation) {
+                      return ScaleTransition(
+                        scale: animation,
+                        child: FadeTransition(
+                          opacity: animation,
+                          child: widget,
+                        ),
+                      );
+                    },
+                    child: Column(
+                      key: ValueKey(_isLogin),
+                      children: [
+                        CupertinoTextField(
+                          controller: _emailController,
+                          placeholder: AppConstants.emailHint,
+                          prefix: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Icon(CupertinoIcons.mail),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 18, horizontal: 16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(color: CupertinoColors.systemGrey),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        CupertinoTextField(
+                          controller: _passwordController,
+                          placeholder: AppConstants.passwordHint,
+                          obscureText: true,
+                          prefix: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Icon(CupertinoIcons.lock),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 18, horizontal: 16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(color: CupertinoColors.systemGrey),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        _isLoading
+                            ? Lottie.asset(
+                          _isLogin
+                              ? AppConstants.loginAnimation
+                              : AppConstants.registerAnimation,
+                          height: 50,
+                          fit: BoxFit.cover,
+                        )
+                            : CupertinoButton(
+                          color: _isLogin
+                              ? CupertinoColors.activeBlue
+                              : primaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                _isLoading = true;
+                              });
+
+                              await Future.delayed(Duration(seconds: 2));
+
+                              if (_isLogin) {
+                                await context.read<AuthViewModel>().loginWithEmail(
+                                  email: _emailController.text,
+                                  password: _passwordController.text,
+                                  context: context,
+                                );
+                              } else {
+                                await context.read<AuthViewModel>().signUpWithEmail(
+                                  email: _emailController.text,
+                                  password: _passwordController.text,
+                                  context: context,
+                                );
+                              }
+
+                              await Future.delayed(Duration(seconds: 1));
+
+                              setState(() {
+                                _isLoading = false;
+                              });
+                            }
+                          },
+                          child: Text(
+                            _isLogin
+                                ? AppConstants.loginButtonText
+                                : AppConstants.registerButtonText,
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: CupertinoColors.white,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    prefixIcon: Icon(Icons.email),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return AppConstants.emailEmptyError;
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: AppConstants.passwordHint,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    prefixIcon: Icon(Icons.lock),
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return AppConstants.passwordEmptyError;
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      if (_isLogin) {
-                        context.read<AuthViewModel>().loginWithEmail(
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                              context: context,
-                            );
-                      } else {
-                        context.read<AuthViewModel>().signUpWithEmail(
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                              context: context,
-                            );
-                      }
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  SizedBox(height: 10),
+                  CupertinoButton(
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(
+                      _isLogin
+                          ? AppConstants.noAccountText
+                          : AppConstants.haveAccountText,
+                      style: TextStyle(color: CupertinoColors.activeBlue),
                     ),
                   ),
-                  child: Text(
-                    _isLogin
-                        ? AppConstants.loginButtonText
-                        : AppConstants.registerButtonText,
-                    style: TextStyle(fontSize: 18),
+                  SizedBox(height: 20),
+                  CupertinoButton(
+                    onPressed: () {
+                      context.read<AuthViewModel>().signInAnonymously(context);
+                    },
+                    child: Text(
+                      AppConstants.continueWithoutLogin,
+                      style: TextStyle(color: CupertinoColors.systemGrey),
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isLogin = !_isLogin;
-                    });
-                  },
-                  child: Text(
-                    _isLogin
-                        ? AppConstants.noAccountText
-                        : AppConstants.haveAccountText,
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    context.read<AuthViewModel>().signInAnonymously(context);
-                  },
-                  child: Text(
-                    AppConstants.continueWithoutLogin,
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
